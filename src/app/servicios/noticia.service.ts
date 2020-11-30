8df9cf031be5575b7a1c90b76ac02b0043b4a061
XIAC <x.i.a.c.1504@gmail.com>
2020-11-30 17:25:00 -0400

    modificaciones en los tabs

---
+++ src/app/servicios/noticia.service.ts
@@ -0,0 +1,14 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Respuesta } from '../modelos/noticia';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class NoticiaService {
+
+  constructor(private http: HttpClient) { }
+  getNoticias(){
+    return this.http.get<Respuesta>(`http://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=4f13656b64774c9ebcc9ca96ee4ba2dd`);
+  }
+}
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  apiKey: '4f13656b64774c9ebcc9ca96ee4ba2dd'
 };
 
 /*
